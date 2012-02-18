054825c4f6d305f2b1b7f62f4271a5d583cf04ff
Michel Carroll <carrmi.0@gmail.com>
2012-02-17 23:14:06 -0500

    WIP

---
+++ src/Nimbus/ApartmentsBundle/Controller/ListingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Nimbus\ApartmentsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ListingController extends Controller
+{
+    
+    public function indexAction()
+    {
+        return $this->render('NimbusApartmentsBundle:Listing:index.html.twig');
+    }
+}
